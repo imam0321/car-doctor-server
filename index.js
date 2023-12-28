7ed0653c53231fd7c0de8981df4aca58c70ddda7
imam0321 <imam.hossain0321@gmail.com>
2023-12-29 01:03:36 +0600

    deploy

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
 app.use(express.json());
+app.use(cors(corsOptions));
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0hgquea.mongodb.net/?retryWrites=true&w=majority`;
 
